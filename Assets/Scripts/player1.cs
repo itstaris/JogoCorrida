9815020d7aef9ba586851b3137f6bee73d4d0105
Tamires <tamires.ei04@gmail.com>
2025-04-07 09:03:42 -0300

    adding acceleration

--- Assets/Scripts/player1.cs
+++ Assets/Scripts/player1.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class player1 : MonoBehaviour
 {
+    private Rigidbody2D rb; // Referência ao Rigidbody2D do jogador
+    public float maxSpeed;
+    public float moveSpeed;
+    public float acceleration;
+    private Vector2 currentVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
         
+        //vetor de direção
+        Vector2 targetVelocity = new Vector2(moveX, moveY).normalized * moveSpeed;
+        
+        //aceleração gradual
+        currentVelocity = Vector2.MoveTowards(currentVelocity, targetVelocity, acceleration * Time.deltaTime);
+        
+        //atualização da velocidade
+        rb.velocity = currentVelocity;
     }
 }
